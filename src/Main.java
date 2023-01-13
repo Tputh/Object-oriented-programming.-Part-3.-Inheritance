4b08ea6c3c74c3fa6321c96473dbce1dd3a2be08
Имя <vitya.klyucherov@mail.ru>
2023-01-13 19:10:02 +0300

    Дз. Объектно-ориентированное программирование. Часть 3. Наследование

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Объектно-ориентированное программирование. Часть 3. Наследование");
+        System.out.println("Дз. Объектно-ориентированное программирование. Часть 3. Наследование");
+        Bus bus = new Bus("Mercedes-Benz", "Travego M OM 457 LA", 2020,
+                "Германия", "Белый", 200,56,
+                12.82,3.7);
+        System.out.println(bus);
+        Car car = new Car("Dodge", "Challenger SRT Demon", 2015,
+                "Америка", "Белый", 335, "механическая",
+                "купе", 6.2);
+        System.out.println(car);
     }
 }
